a193f82f4c16b63e9b3494f546cfa6b1fc928e08
Balaji0112 <60376123+Balaji0112@users.noreply.github.com>
2020-02-15 16:01:37 +0530

    Create Isunique(o(n)).py

---
+++ Isunique(o(n)).py
@@ -0,0 +1,13 @@
+def IsUnique(word):
+    Checker=0
+    for i in range(len(word)):
+        Index=ord(word[i])-ord('a')
+        if (Checker & (1<<Index)) >0:
+            return False
+        Checker=Checker|(1<<Index)
+    return True
+word=input()
+if(IsUnique(word)):
+    print(word+'has all the characters as unique')
+else:
+    print(word+'has some repeated letters')
