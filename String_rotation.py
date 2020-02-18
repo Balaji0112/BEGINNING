8e32e80342c93dc3998a801d2df260513df6b4f2
Balaji0112 <60376123+Balaji0112@users.noreply.github.com>
2020-02-18 11:47:45 +0530

    String_rotation

---
+++ String_rotation.py
@@ -0,0 +1,27 @@
+def Rotated_string(word1,word2):
+    length=len(word1)
+    temp=[0]*length
+    for i in range(length):
+        j=i
+        k=0
+        while(j<len(word1)):
+            temp[k]=word1[j]
+            k+=1
+            j+=1
+        j=0
+        while(j<i):
+            temp[k]=word1[j]
+            j+=1
+            k+=1
+        s=''.join(temp)
+        if word2 == s:
+           return True
+    return False
+word1,word2=input().split()
+rotate=Rotated_string(word1,word2)
+if(rotate):
+    print('yes word1 is a substring of word2')
+else:
+    print('no word1 is not a substring of word2')
+
+
